4f11c085de9b0aa1a334076df21fb26a66172b82
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2022-05-12 13:46:22 +0200

    ADDED controller, routes, app to backend

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from 'cors'
+
+import postRoutes from './routes/posts.js'
+
+const app = express();
+
+app.use('/posts', postRoutes)
+
+app.use(bodyParser.json({limit:"30mb", extended:true}));
+app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://maxinger:11HMjP69Jzy39Rnv@cluster0.bk9lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
