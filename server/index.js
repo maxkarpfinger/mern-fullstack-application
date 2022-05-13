2076ac7bfdcb33a6e7783b13496ab7a0a19ddca4
maxkarpfinger <50171965+maxkarpfinger@users.noreply.github.com>
2022-05-13 14:02:36 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://maxinger:11HMjP69Jzy39Rnv@cluster0.bk9lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+const CONNECTION_URL = "mongodb+srv://user:password@cluster0.bk9lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL)
