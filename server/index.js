c72bb601b19501bd7b725cc4dad575c0e671981a
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2022-05-13 14:12:10 +0200

    CHANGED login credentials for db to be hidden

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
-
+import {LOGIN} from "./constants/password.js";
 import postRoutes from './routes/posts.js';
 
 const app = express();
@@ -13,7 +13,8 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = "mongodb+srv://maxinger:11HMjP69Jzy39Rnv@cluster0.bk9lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+const CONNECTION_URL = `mongodb+srv://${LOGIN}@cluster0.bk9lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL)
