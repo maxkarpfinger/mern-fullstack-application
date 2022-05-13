3381430596f4ce7ab830b67bbbc74caf263e2bba
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2022-05-13 11:06:46 +0200

    FIXED bug with cors and proxy, ADDED setupProxy, ADDED redux and actions

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ import express from 'express'
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from 'cors'
-
 import postRoutes from './routes/posts.js'
-
 const app = express();
 
-app.use('/posts', postRoutes)
+
 
 app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
-app.use(cors());
+const corsOptions = {
+    origin: 'http://localhost:3000',
+    credentials: true,
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
+app.use('/posts', postRoutes)
+
 
 const CONNECTION_URL = "mongodb+srv://maxinger:11HMjP69Jzy39Rnv@cluster0.bk9lk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000;
