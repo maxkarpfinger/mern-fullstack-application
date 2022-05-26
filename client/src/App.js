70ef0fdd0aa2e4381a3d7301e927e369392f036a
Maximilian Karpfinger <maximilian.karpfinger@tum.de>
2022-05-26 13:51:51 +0200

    ADDED reducer for auth, however some authenticating library for google seems to be deprecated, needs fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Container} from '@material-ui/core';
-import {BrowserRouter, Switch, Route} from "react-router-dom";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home"
+import Auth from "./components/Auth/Auth";
 
 const App = () => {
     return (
         <BrowserRouter>
             <Container maxWidth="lg">
                 <Navbar/>
-                <Switch>
-                    <Route path="/" exact component={Home}/>
-                    <Route path="/auth" exact component={Auth}/>
-                </Switch>
-                <Home/>
+                <Routes>
+                    <Route path="/" exact element={<Home/>}/>
+                    <Route path="/auth" exact element={<Auth/>}/>
+                </Routes>
             </Container>
         </BrowserRouter>
     );
